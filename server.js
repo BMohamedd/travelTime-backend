526a594c058307e45fbb9dcef59946a6a05f7066
Belhadj Mohamed <rafikgamer8@gmail.com>
2022-02-28 10:29:18 +0100

    changes in the routing

--- server.js
+++ server.js
@@ -10,14 +10,7 @@ app.use(express.json({limit:'2mb'}));
 app.use(express.urlencoded({ extended: true, limit: "2mb"}));
 app.use(cors())
 //routes
-app.use("/server", require('./routes/routes'));
-
-// if (process.env.NODE_ENV == "production") {
-//     app.use(express.static("client/build"));
-//     app.get('/', (req, res) => {
-//         res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-//     })   
-// }
+app.use(require('./routes/routes'));
 
 
 const port = process.env.PORT || 5000;
